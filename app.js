f4a505a87702f409d69a56e3c1aab93f5ec273e9
vmarmousez <valery.marmousez@gmail.com>
2016-10-06 21:32:40 +0200

    Improvements

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 var nodemailer = require('nodemailer');
+var _ = require('underscore');
+var fs = require('fs');
+var path = require('path');
+
 var smtpConfig = require('./mailConfig.js');
-var argumentParser = require("node-argument-parser");
-
-var argv = argumentParser.parse("./arguments.json", process);
-
-console.log(`"${argv.sender}" <${argv.senderMail}>`);
+var config = require('./config.js');
 
 var transporter = nodemailer.createTransport(smtpConfig);
 
-var mailOptions = {
-    from: `"${argv.sender}" <${argv.senderMail}>`,
-    to: argv.receiver,
-    subject: argv.subject,
-    text: argv.content
-};
+var sendMailWithAttachment = (fileName) => {
+  _.each(config.receivers, receiver => {
+    var mailOptions = {
+        from: `"${config.sender.name}" <${config.sender.mail}>`,
+        to: receiver,
+        subject: config.subject,
+        text: config.content,
+        attachments: [
+            {
+                path: fileName
+            }
+         ]
+    };
 
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+  });
+}
+
+var getNewestFile = () => {
+    var dir = config.directory;
+    var files = fs.readdirSync(dir);
+    var newest = _.max(_.filter(files, file => file.indexOf(config.fileExtension) !== -1), f => {
+        var fullpath = path.join(dir, f);
+        return fs.statSync(fullpath).ctime;
+    });
+    if (newest !== -Infinity) {
+      return path.join(dir, newest);
+    } else {
+      return -Infinity;
     }
-    console.log('Message sent: ' + info.response);
-});
+}
+
+var newestFile = getNewestFile();
+if (newestFile !== -Infinity) {
+    sendMailWithAttachment(newestFile);
+} else {
+    console.log(`No file found in ${config.directory} matching pattern *${config.fileExtension}`);
+}
